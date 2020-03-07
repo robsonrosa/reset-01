d7951532bba8fcfe24787c26326d9f72eca38a1d
robsonrosa <robsonrosa@cwi.com.br>
2020-03-07 12:12:43 -0300

    Removendo mensagens estranhas

--- src/App.java
+++ src/App.java
@@ -2,8 +2,5 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("outras coisas");
-        System.out.println("sem criatividade");
-        System.out.println("pra mensagens");
     }
 }
