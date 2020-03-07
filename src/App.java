85b8cd1fb0cd857baaf334022c6c1ab49c8cb9db
robsonrosa <robsonrosa@cwi.com.br>
2020-03-07 10:55:13 -0300

    criação do projeto

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("outras coisas");
+        System.out.println("sem criatividade");
+        System.out.println("pra mensagens");
+    }
+}
